ae1e33cfebf4039bc3e1fd9429381988242a9826
Alizaibalam <alizaibalam@hotmail.com>
2018-05-17 10:50:43 +1000

    Updated to include multiple Machines.

--- Removing Unwanted Tags.ps1
+++ Removing Unwanted Tags.ps1
@@ -1,17 +1,20 @@
-$VMtags = (Get-AzureRmVM -ResourceGroupName snrg01 -Name snvm02).tags
+$VMS = Get-AzureRmVM -ResourceGroupName snrg01
 $TagtoRemove = @{Key="TestVmName";Value="abcd"}
-$newtag = @{}
-
-foreach ( $KVP in $VMtags.GetEnumerator() )
-{ 
-     Write-Host "`n`n`n"
-     If($KVP.Key -eq $TagtoRemove.Key){ write-host $Key "exists in "  $TagtoRemove.Key "`n"}
-     Else 
-     { 
+foreach ($VM in $VMs)
+{
+    $VMtags = $VM.tags
+    $newtag = @{}
+    foreach ( $KVP in $VMtags.GetEnumerator() )
+    { 
+         Write-Host "`n`n`n"
+         If($KVP.Key -eq $TagtoRemove.Key){ write-host $Key "exists in "  $TagtoRemove.Key "`n"}
+         Else 
+         { 
      
-     write-host " Adding " $KVP
-     $newtag.add($KVP.Key,$KVP.Value)
+            write-host " Adding " $KVP
+            $newtag.add($KVP.Key,$KVP.Value)
+         }
+
      }
-
-
+    Set-AzureRmResource -ResourceGroupName $VM.ResourceGroupName -ResourceName $VM.Name -Tag $newtag -Force -ResourceType Microsoft.Compute/VirtualMachines
 }
