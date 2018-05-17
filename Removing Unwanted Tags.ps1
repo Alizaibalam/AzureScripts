61518a79f16ea948a3e508fe56153bb73bee9af0
Alizaibalam <alizaibalam@hotmail.com>
2018-05-17 10:55:52 +1000

    Added Comments to the script

--- Removing Unwanted Tags.ps1
+++ Removing Unwanted Tags.ps1
@@ -1,20 +1,28 @@
+#Getting the list of VMs based on the resource group. THe Scope can be changed to include more resources.
+
 $VMS = Get-AzureRmVM -ResourceGroupName snrg01
+
+#Details of the tag to remove are stored in the $TagtoRemove variable.
+
 $TagtoRemove = @{Key="TestVmName";Value="abcd"}
+
 foreach ($VM in $VMs)
 {
-    $VMtags = $VM.tags
-    $newtag = @{}
+    $VMtags = $VM.tags # Getting the list of all the tags for the VM.
+    $newtag = @{} # Creating a new Hashtable variable to store the Tag Values.
     foreach ( $KVP in $VMtags.GetEnumerator() )
     { 
          Write-Host "`n`n`n"
-         If($KVP.Key -eq $TagtoRemove.Key){ write-host $Key "exists in "  $TagtoRemove.Key "`n"}
+         If($KVP.Key -eq $TagtoRemove.Key)
+         { 
+           write-host $TagtoRemove.Key "exists in the "$VM.Name " will be removed `n"}
          Else 
          { 
      
-            write-host " Adding " $KVP
-            $newtag.add($KVP.Key,$KVP.Value)
+           $newtag.add($KVP.Key,$KVP.Value) # Adding all the tags in the $newtag Variable except the $TagtoRemove.key values 
          }
 
      }
+    #Updating the Virtual machine with the updated tag values $newtag.
     Set-AzureRmResource -ResourceGroupName $VM.ResourceGroupName -ResourceName $VM.Name -Tag $newtag -Force -ResourceType Microsoft.Compute/VirtualMachines
 }
